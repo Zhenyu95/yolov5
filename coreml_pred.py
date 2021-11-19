916d9ad81902bf2eafbe1b3bb704a2f5a8119846
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2021-11-19 14:58:51 +0800

    updated coreml_pred.py
    fixed bug of showing wrong bounding box

--- coreml_pred.py
+++ coreml_pred.py
@@ -16,7 +16,11 @@ SAVE_TXT = True
 CAT_NAMES = ['Screw', 'unknown']
 COLORS = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(CAT_NAMES))]
 PATH = "./"
-ANCHORS = ([116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]) # from <model>.yml
+# ANCHORS = ([116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]) # from <model>.yml
+ANCHORS = ([2.375,3.375, 5.5,5, 4.75,11.75], 
+           [6,4.25, 5.375,9.5, 11.25,8.5625], 
+           [4.375,9.40625, 9.46875,8.25, 7.43750,16.93750],
+           [6.81250,9.60938, 11.54688,5.93750, 14.45312,12.37500])
 IMG_SIZE = (2304, 3072)
 nc = len(CAT_NAMES)
 nl = len(ANCHORS)
@@ -24,8 +28,9 @@ na = len(ANCHORS[0]) // 2
 no = nc + 5  # number of outputs per anchor
 grid = [torch.zeros(1)] * nl  # init grid
 a = torch.tensor(ANCHORS).float().view(nl, -1, 2)
-anchor_grid = a.clone().view(nl, 1, -1, 1, 1, 2)
-stride = [32, 8, 16] # check your model config
+anchor_grid = [torch.zeros(1)] * nl
+# anchor_grid = a.clone().view(nl, 1, -1, 1, 1, 2)
+stride = [8, 16, 32, 64] # check your model config
 conf_thres = .2
 
 
@@ -47,9 +52,11 @@ def load_image(path, resize_to=None):
     img_np = np.array(img).astype(np.float32)
     return img_np, img
 
-def make_grid(nx=20, ny=20):
+def make_grid(nx=20, ny=20, i=0):
     yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
-    return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
+    grid = torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
+    anchor_grid = (a[i] * stride[i]).view((1, na, 1, 1, 2)).expand(1, na, ny, nx, 2).float()
+    return grid, anchor_grid
 
 
 def resize_image(source_image):
@@ -63,28 +70,26 @@ def resize_image(source_image):
 
 def eval(file_name):   
     source = PIL.Image.open(os.path.join(IMAGE_FOLDER, file_name))
-    resized = resize_image(source)
+    resized = resize_image(source.copy())
 
     predictions = model.predict({'image': resized})
 
     z = []  # inference output
     x = []
-    for pred in predictions:
-        x.append(torch.Tensor(predictions[pred]))
-    x.reverse()
+    for head in ['var_1763', 'var_1778', 'var_1793', 'var_1808']:
+        x.append(torch.Tensor(predictions[head]))
 
     for i in range(nl):
         bs, _, ny, nx, _ = x[i].shape
 
-        if grid[i].shape[2:4] != x[i].shape[2:4]:
-            grid[i] = make_grid(nx, ny)
+        # if grid[i].shape[2:4] != x[i].shape[2:4]:
+        grid[i], anchor_grid = make_grid(nx, ny, i)
 
         y = x[i].sigmoid()
         y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + grid[i]) * stride[i]  # xy
-        y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * anchor_grid[i]  # wh
+        y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * anchor_grid  # wh
         z.append(y.view(bs, -1, no))
     
-    # pred = (torch.cat(z, 1), x)[0]
     pred = torch.cat(z, 1)
 
     pred = non_max_suppression(pred, conf_thres, .3, classes=None, agnostic=False)
@@ -92,44 +97,38 @@ def eval(file_name):
     # Process detections
     for i, det in enumerate(pred):  # detections per image
         p, s = "./", ""
+        label=[]
 
         if det is not None and len(det):
             # Rescale boxes from img_size to im0 size
-            # det = det[((det[:, 0]-det[:, 2])*(det[:, 1]-det[:, 3])) > 80]
-
-            # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += '%g %ss, ' % (n, CAT_NAMES[int(c)])  # add to string
-
-            # Write results
+            det = det[((det[:, 0]-det[:, 2])*(det[:, 1]-det[:, 3])) > 80]
             for *xyxy, conf, cls in det:
-                if SAVE_TXT:  # Write to file
-#                     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)/np.array([3024, 4032, 3024, 4032]))).view(-1).tolist()
-                    # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)/torch.tensor([2304, 3072, 3024, 4032]).view(1,4))).view(-1).tolist()
+                if SAVE_TXT:
                     xywh = xyxy2xywhn(torch.tensor(xyxy).view(1, 4), w=IMG_SIZE[0], h=IMG_SIZE[1]).view(-1).tolist()
-                    with open(os.path.join(OUT_FOLDER, '{}.txt'.format(file_name[:-4])), 'a') as f:
-                        f.write(('%g ' * 5 + '\n') % (cls, *xywh))
+                    label.append(('%g ' * 5 + '\n') % (cls, *xywh))
                 if SAVE_IMG:
                     draw = PIL.ImageDraw.Draw(resized)
-                    # xyxyn = xywhn2xyxy(torch.tensor(xywh).view(1, 4), w=IMG_SIZE[0], h=IMG_SIZE[1]).type(torch.uint8).view(2, 2)
-                    # print(xyxyn)
                     draw.rectangle(np.array(torch.tensor(xyxy).view(2,2)), outline='red')
-                    resized.save(os.path.join(OUT_FOLDER, '{}.jpg'.format(file_name[:-4])))
+        if SAVE_TXT:
+            with open(os.path.join(OUT_FOLDER, '{}.txt'.format(file_name[:-4])), 'a') as f:
+                for line in label:
+                    f.write(line)
+        if SAVE_IMG:
+            resized.save(os.path.join(OUT_FOLDER, '{}.jpg'.format(file_name[:-4])))
 
 def debug():
     global model
-    # if os.path.exists(OUT_FOLDER):
-    #     shutil.rmtree(OUT_FOLDER)
-    # os.makedirs(OUT_FOLDER)
+    if os.path.exists(OUT_FOLDER):
+        shutil.rmtree(OUT_FOLDER)
+    os.makedirs(OUT_FOLDER)
 
     # Load the model
     model = coremltools.models.model.MLModel(COREML_MODEL)
 
-    # for images in os.listdir(IMAGE_FOLDER):
-    #     if images.endswith(".jpg"):
-    #         eval(images)
-    eval('P9YI6XTHXQ.jpg')
+    for images in os.listdir(IMAGE_FOLDER):
+        if images.endswith(".jpg"):
+            eval(images)
+    # eval('1_4032.jpg')
     
     
 def main():
