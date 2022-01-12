2b57730e38863ccf78d7ce0e244164e1461d63ac
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2022-01-12 14:42:10 +0800

    updated bash

--- training.sh
+++ training.sh
@@ -3,8 +3,8 @@ mv /Users/zhenyu/Desktop/dataset_1230.zip /root/
 cd ..
 unzip dataset_1230.zip
 cd yolov5
-#python3 -m torch.distributed.launch --nproc_per_node 8 train.py --batch 32 --data iphoneaoi.yaml --weights yolov5x.pt --device 0,1,2,3,4,5,6,7 --epochs 30 --img 1920 --rect
-for i in 0 1 2 3 4 5 6 7; do
-  nohup python3 train.py --epochs 30 --data iphoneaoi.yaml --weights yolov5x.pt --cache --evolve 1000 --device $i > evolve_gpu_$i.log &
-done
-#python3 export.py --data data/iphoneaoi.yaml --weights runs/train/exp/weights/best.pt --img 1920 1440 --include coreml
+python3 -m torch.distributed.launch --nproc_per_node 8 train.py --batch 32 --data iphoneaoi.yaml --weights yolov5x.pt --device 0,1,2,3,4,5,6,7 --epochs 80 --img 1920 --rect
+# for i in 0 1 2 3 4 5 6 7; do
+#   nohup python3 train.py --epochs 30 --data iphoneaoi.yaml --weights yolov5x.pt --cache --evolve 1000 --device $i > evolve_gpu_$i.log &
+# done
+python3 export.py --data data/iphoneaoi.yaml --weights runs/train/exp/weights/best.pt --img 1920 1440 --include coreml
