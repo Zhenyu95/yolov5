5c9ed09044d4bb1f94251d99c0400779f9e305d5
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2022-01-26 17:13:30 +0800

    updated random_move

--- random_move.py
+++ random_move.py
@@ -8,5 +8,5 @@ file_list = [f[:-4] for f in os.listdir('/root/dataset_test_NG/images/test/') if
 rand_list = random.sample(file_list, 100)
 
 for each in rand_list:
-    shutil.move('/root/dataset_test_NG/images/test/'+each+'.jpg', '/root/dataset_1230/labels/train/'+each+'.jpg')
+    shutil.move('/root/dataset_test_NG/images/test/'+each+'.jpg', '/root/dataset_1230/images/train/'+each+'.jpg')
     shutil.move('/root/dataset_test_NG/labels/test/'+each+'.txt', '/root/dataset_1230/labels/train/'+each+'.txt')
