10b5f34ea15755c329503be7415b3edb99fb8e8f
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2022-01-27 14:35:51 +0800

    updated bash and hyperparameters

--- training.sh
+++ training.sh
@@ -17,7 +17,7 @@ unzip dataset_test_NG.zip
 rm dataset_1230/images/test/*
 rm dataset_1230/labels/test/*
 
-# python3 /root/yolov5/random_move.py
+python3 /root/yolov5/random_move.py
 
 mv dataset_test_OK/images/test/* dataset_1230/images/test/
 
@@ -28,7 +28,7 @@ mv 496/* ./dataset_1230/images/train/
 
 cd yolov5
 # python3 train.py --epochs 100 --data iphoneaoi.yaml --batch 32 --weights yolov5x.pt --img 1920 --cache --evolve 1000
-python3 -m torch.distributed.launch --nproc_per_node 4 train.py --batch 24 --data iphoneaoi.yaml --weights yolov5x.pt --device 0,1,2,3 --epochs 300 --img 1920 --rect
+python3 -m torch.distributed.launch --nproc_per_node 4 train.py --batch 24 --data iphoneaoi.yaml --weights yolov5x.pt --device 0,1,2,3 --epochs 300 --img 1920 --rect --adam
 # for i in 0 1 2 3 4 5 6 7; do
 #   nohup python3 train.py --epochs 50 --data iphoneaoi.yaml --weights yolov5x.pt --cache --evolve 500 --batch 32 --img 1920 --rect --device $i > evolve_gpu_$i.log &
 # done
