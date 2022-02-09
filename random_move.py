d4a50ed9d451e436a404384f9aafff10d0c56864
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2022-02-09 10:28:01 +0800

    updated bash

--- random_move.py
+++ random_move.py
@@ -4,15 +4,15 @@ import os
 from tqdm import tqdm
 
 random.seed(9)
-img_path = '/root/dataset_test_NG/images/test/'
-label_path = '/root/dataset_test_NG/labels/test/'
-img_dst = '/root/dataset_1230/images/train/'
-label_dst = '/root/dataset_1230/labels/train/'
+# img_path = '/root/dataset_test_NG/images/test/'
+# label_path = '/root/dataset_test_NG/labels/test/'
+# img_dst = '/root/dataset_1230/images/train/'
+# label_dst = '/root/dataset_1230/labels/train/'
 
-# img_path = '/Users/zhenyu/Desktop/dataset_test_NG/images/test/'
-# label_path = '/Users/zhenyu/Desktop/dataset_test_NG/labels/test/'
-# img_dst = '/Users/zhenyu/Desktop/untitled/'
-# label_dst = '/Users/zhenyu/Desktop/untitled/'
+img_path = '/Users/zhenyu/Desktop/dataset_test_NG/images/test/'
+label_path = '/Users/zhenyu/Desktop/dataset_test_NG/labels/test/'
+img_dst = '/Users/zhenyu/Desktop/dataset_1230/images/train/'
+label_dst = '/Users/zhenyu/Desktop/dataset_1230/labels/train/'
 
 file_list = [f[:-4] for f in os.listdir(img_path) if f.endswith('.jpg')]
 rand_list = random.sample(file_list, 100)
