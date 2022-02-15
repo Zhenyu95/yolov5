857774e90b8c6b84a49d40054a03e183c22c2db3
Zhenyu95 <61449922+Zhenyu95@users.noreply.github.com>
2022-02-15 14:35:57 +0800

    updated hyperparams

--- training.sh
+++ training.sh
@@ -28,9 +28,9 @@ mv 496/* ./dataset_1230/images/train/
 
 cd yolov5
 # python3 train.py --epochs 1000 --data iphoneaoi.yaml --batch 16 --weights yolov5x.pt --img 1920 --cache --evolve 1000 --rect --adam
-python3 -m torch.distributed.launch --nproc_per_node 4 train.py --batch 24 --data iphoneaoi.yaml --weights yolov5x.pt --device 0,1,2,3 --epochs 600 --img 1920 --rect --adam
+python3 -m torch.distributed.launch --nproc_per_node 8 train.py --batch 48 --data iphoneaoi.yaml --cfg models/hub/yolov5s-transformer.yaml --device 0,1,2,3,4,5,6,7 --epochs 600 --img 1920 --rect --adam
 # for i in 0 1 2 3 4 5 6 7; do
 #   nohup python3 train.py --epochs 50 --data iphoneaoi.yaml --weights yolov5x.pt --cache --evolve 500 --batch 32 --img 1920 --rect --device $i > evolve_gpu_$i.log &
 # done
-# python3 export.py --data data/iphoneaoi.yaml --weights runs/train/exp/weights/best.pt --img 1920 1440 --include coreml
+python3 export.py --data data/iphoneaoi.yaml --weights runs/train/exp/weights/best.pt --img 1920 1440 --include coreml
 # zip -r runs.zip runs/
